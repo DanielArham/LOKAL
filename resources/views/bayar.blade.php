b55d5d87dcc6a76b04b9ed0b22622646d82c927c
intannurrh <intannur270@gmail.com>
2023-12-10 18:24:56 +0700

    dancok

--- resources/views/bayar.blade.php
+++ resources/views/bayar.blade.php
@@ -13,80 +13,143 @@
 <body>
 
 <section>
-<div class="wrap py-3">
-        <div class="container-lg">
-            <div class="d-flex justify-content-between mb-2">
-                <h2>
-                    Pembayaran
-                </h2>
-            </div>
-            <div class="row">
-                <div class="col-md-7">
-                    <div class="callout callout-info bg-white py-2 my-3">
-                        <p class="lead mb-0">
-                            <span class="text-secondary">Total Pembayaran: </span>
-                            <small class="align-top text-secondary">Rp</small>1.700.000
+
+<div class="d-flex justify-content-center">
+        <div class="wrap py-3 col-md-7">
+        <div class="card features-2 shadow-lg">
+            <div class="container-lg">
+                <div class="jumbotron py-3 mb-3 text-center shadow-sm">
+                    <h1 class="text-monospace">Pembayaran</h1>
+                    <p class="lead mb-0">
+                        <span class="text-secondary">Total Pembayaran: </span>
+                        <small class="align-top text-secondary">Rp</small>1.700.000
+                    </p>
+                </div>
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <p class="lead text-center mb-4">
+                            Batas Waktu Pembayaran <br>
+                            <span class="text-danger" id="limit"></span> <br>
+                            <small> Anda dapat melakukan pembayaran dengan cara sebagai berikut:</small>
                         </p>
                     </div>
-                    <div class="jumbotron py-3 mb-3 bg-blue-dark text-center shadow-sm">
-                        <h1 class="text-monospace text-yellow">Pembayaran</h1>
-                    </div>
-                    <div class="card shadow-sm">
-                        <div class="card-body">
-                            <p class="lead text-center">
-                                Batas Waktu Pembayaran <br>
-                                <span class="text-danger" id="limit"></span> <br>
-                                <small>Segera lakukan pembayaran sebelum waktu habis.</small>
-                            </p>
-                            <h1 class="text-center mb-5" id="clock"></h1>
-                            <p class="mb-0">
-                                Anda dapat melakukan pembayaran dengan cara sebagai berikut:
-                            </p>
-                        </div>
-                        <ul id="how-to-pay" class="accordion list-group list-group-flush">
-                            <li class="list-group-item">
-                                <a href="#atm-bni" class="text-decoration-none stretched-link" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="atm-bni">ATM BNI</a>
-                            </li>
-                            <div id="atm-bni" class="card-body collapse" aria-labelledby="atm-bni" data-parent="#how-to-pay">
-                                <ol>
-                                    <li>Transfer ke rek 1234567899 atas nama danial w</li>
-                                </ol>
-                            </div>
                 </div>
+            </div>
         </div>
-    </div>
+
+        </div>  </div>
+
+    <script type="text/javascript">
+
+    var limit = new Date().toLocaleString("en-US", {timeZone: "Asia/Jakarta"}).slice(0,10).replace(/\//g,'-') + " 23:59:59";
+    // Set the date we're counting down to
+    var countDownDate = new Date(limit).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        hours = (hours < 10) ? '0' + hours : hours;
+        document.getElementById("limit").innerHTML = limit;
+        document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("clock").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+    </script>
 </section>
 <section id="why-us" class="container section">
-      <h1 class="text-center">Kenapa Memilih Kami ?</h1><br><br>
       <div class="row equal">
           <div class="col-12 col-md-4 spc">
               <div class="card features-2 shadow-lg">
                   <span class="text-center content">
-                      <img src="{{ asset('images/kreatif.png') }}" style="width: 100px;">
-                      <h4>Desain Kreatif</h4>
-                      <p>Kami membuat desain yang <b>unik</b> dan <b>menarik</b> untuk setiap produk yang kami jual.</p>
+                      <img src="{{ asset('images/bni.jpg') }}" style="width: 100px;">
+                      <h4>Transfer Bank BNI</h4>
+                      <strong>No Rekening:</strong> 5678-123-456
                   </span>
               </div>
           </div>
           <div class="col-12 col-md-4 spc">
               <div class="card features-2 shadow-lg">
                   <span class="text-center content">
-                      <img src="{{ asset('images/price.png') }}" style="width: 130px;">
-                      <h4>Harga Terjangkau</h4>
-                      <p>Kami menyediakan produk kualitas <b>terbaik</b> dengan harga yang <b>ramah dikantong</b>. </p>
+                      <img src="{{ asset('images/bri.jpg') }}" style="width: 130px;">
+                      <h4>Transfer Bank BRI</h4>
+                      <strong>No Rekening:</strong> 5678-123-456
                   </span>
               </div>
           </div>
           <div class="col-12 col-md-4 spc">
               <div class="card features-2 shadow-lg">
                   <span class="text-center content">
-                      <img src="{{ asset('images/proses.png') }}" style="width: 120px;">
-                      <h4>Proses Cepat</h4>
-                      <p>Kami memroses desain yang anda pesan <b>secepat kilat</b>.</p>
+                      <img src="{{ asset('images/bca.jpg') }}" style="width: 120px;">
+                      <h4>Transfer Bank BCA</h4>
+                      <strong>No Rekening:</strong> 5678-123-456
+                  </span>
+              </div>
+          </div>
+      </div>
+
+      <div class="row equal">
+          <div class="col-12 col-md-4 spc">
+              <div class="card features-2 shadow-lg">
+                  <span class="text-center content">
+                      <img src="{{ asset('images/dana.jpg') }}" style="width: 100px;">
+                      <h4>Transfer Dana</h4>
+                      <strong>No HP:</strong> 5678-123-456
+                  </span>
+              </div>
+          </div>
+          <div class="col-12 col-md-4 spc">
+              <div class="card features-2 shadow-lg">
+                  <span class="text-center content">
+                      <img src="{{ asset('images/shopeepay.jpg') }}" style="width: 130px;">
+                      <h4>Transfer ShopeePay</h4>
+                      <strong>No HP:</strong> 5678-123-456
+                  </span>
+              </div>
+          </div>
+          <div class="col-12 col-md-4 spc">
+              <div class="card features-2 shadow-lg">
+                  <span class="text-center content">
+                      <img src="{{ asset('images/gopay.jpg') }}" style="width: 120px;">
+                      <h4>Transfer Gopay</h4>
+                      <strong>No HP:</strong> 5678-123-456
                   </span>
               </div>
           </div>
       </div>
   </section>
+
+  <div class="container">
+  <div class="col-12 spc">
+              <div class="card features-2 shadow-lg">
+                  <span class="text-center content">
+                  <h4 class="mb-0">
+                                Silahkan Konfirmasi Pada Nomor Di Bawah Jika Sudah Melakukan Pembayaran dan Kirim Bukti Pembayaran
+</h4> <br><br>
+                            <a href="https://wa.me/085856267775" class="btn btn-info" target="_blank">
+                                Hubungi via WhatsApp
+                            </a>
+                  </span>
+              </div>
+          </div>
+    </div>
+    <br>
+
 </body>
 </html>
