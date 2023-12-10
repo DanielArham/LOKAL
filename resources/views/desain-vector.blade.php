1477fceaf1952758221b2836de9ff3c2b5638b3a
intannurrh <intannur270@gmail.com>
2023-12-10 20:04:29 +0700

    bwfejhadv

--- resources/views/desain-vector.blade.php
+++ resources/views/desain-vector.blade.php
@@ -12,8 +12,7 @@
     <link href="css/bootstrap.css" rel="stylesheet" />
     <link href="css/fahli.css" rel="stylesheet" />
     <link rel="icon" type="image/x-icon" href="{{ asset('images/logokecil.png') }}" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    
   </head>
 
   <body>
@@ -197,10 +196,10 @@
           const params = new URLSearchParams();
           params.append('serviceName', serviceName);
           params.append('price', price);
-  
+
           // Menggabungkan URL dengan parameter
           const urlWithParams = `${url}?${params.toString()}`;
-  
+
           // Mengarahkan pengguna ke halaman checkout dengan parameter serviceName dan price
           window.location.href = urlWithParams;
       }
