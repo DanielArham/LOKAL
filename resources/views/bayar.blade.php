3e79055919c2d2257d7a8fc2df447438d36f784a
intannurrh <intannur270@gmail.com>
2023-12-10 15:22:09 +0700

    ppp

---
+++ resources/views/bayar.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="LOKAL." />
+    <title>Jasa Desain Grafis | LOKAL.</title>
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/fahli.css" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/logokecil.png') }}">
+</head>
+<body>
+
+</body>
+</html>
